b800aeed4938b631acd75d916a2367562a050c30
Jared Heath <silkrawnstudios@gmail.com>
2013-07-06 15:31:17 -0400

    [1.1] Remove standalone logger

--- src/ss/linearlogic/homc/HandsOffMyChest.java
+++ src/ss/linearlogic/homc/HandsOffMyChest.java
@@ -3,14 +3,14 @@ package ss.linearlogic.homc;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HandsOffMyChest extends JavaPlugin {
-	private YeOldeListener listener = new YeOldeListener();
 
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(this.listener, this);
-		YeOldeLogger.logInfo("Plugin successfully enabled! No more unwanted groping of chests!");
+		getLogger().info("Registering listener...");
+		getServer().getPluginManager().registerEvents(new YeOldeListener(), this);
+		getLogger().info("Enabled! No more unwanted groping of chests!");
 	}
 
 	public void onDisable() {
-		YeOldeLogger.logInfo("Plugin successfully disabled!");
+		getLogger().info("Disabled!");
 	}
 }
